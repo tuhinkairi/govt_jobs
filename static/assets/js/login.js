6b2752bd118aa463b07917156cb6bd7024c768ac
tuhin kairi <tuhinkairii@gamil.com>
2023-10-14 13:52:33 +0530

    commit

--- static/assets/js/login.js
+++ static/assets/js/login.js
@@ -1,15 +1,15 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".forms-container");
+const container = document.querySelector(".container");
 const loginForm = document.getElementById("signIn");
 const signupForm = document.getElementById("signUp");
 
 sign_up_btn.addEventListener("click", () => {
-  container.classList.add("signUp-btn");
+  container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("signIn-btn");
+  container.classList.remove("sign-up-mode");
 });
 
 // Event listener for login form submission
@@ -20,5 +20,22 @@ loginForm.addEventListener("submit", async (e) => {
 
   console.log(
     "Login: Username - " + loginuserName + ", Password - " + password
+  );
+});
+
+// Event listener for sign-up form submission
+signupForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = document.getElementById("signupUsername").value;
+  const email = document.getElementById("signupMail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  console.log(
+    "Sign Up: Name - " +
+      name +
+      ", Email - " +
+      email +
+      ", Password - " +
+      password
   );
 });
